73d7e8123bddb3103a3ae304ea18b15e2b8ed127
wb135 <wb135@yingu.com>
2019-08-19 19:49:25 +0800

    2.引入ElementUI及图片资源

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUi from 'element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式"
 Vue.config.productionTip = false
 
+Vue.use(ElementUi)
 new Vue({
   router,
   render: h => h(App)
