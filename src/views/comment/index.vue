19827d5ef0c0214c2899e5bcba3919bcfdd67161
wb135 <wb135@yingu.com>
2019-08-22 21:24:00 +0800

    11.评论列表打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,22 +1,23 @@
 <template>
-<el-card>
-    <breadCommon slot='header'>
+  <el-card>
+    <breadCommon slot="header">
       <!-- 面包屑的插槽 具名插槽  title-->
-        <template slot='title'>评论列表</template>
+      <template slot="title">评论列表</template>
     </breadCommon>
 
-    <el-table :data="list" stripe >
-         <!-- prop属性是绑定的字段名称 -->
-          <el-table-column prop='title' width='500' label='标题'></el-table-column>
-         <el-table-column :formatter="formatter" prop='comment_status' label='评论状态'></el-table-column>
-          <el-table-column prop='total_comment_count' label='总评论数'></el-table-column>
-        <el-table-column prop='fans_comment_count' label='粉丝评论数'></el-table-column>
-     <el-table-column  label='操作'>
-       <el-button type="text">修改</el-button>
-       <el-button type="text">关闭评论</el-button>
-     </el-table-column>
-   </el-table>
-</el-card>
+    <el-table :data="list" stripe>
+      <!-- prop属性是绑定的字段名称 -->
+      <el-table-column prop="title" width="500" label="标题"></el-table-column>
+      <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <el-button type="text">修改评论</el-button>
+          <el-button @click="closeOrOpen(obj.row)" :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}"   type="text" >{{ obj.row.comment_status ? "关闭评论" : "打开评论" }}</el-button></template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
@@ -24,10 +25,22 @@ export default {
   data () {
     return {
       list: []
-
     }
   },
   methods: {
+    closeOrOpen (row) {
+      let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您确定要${mess}评论吗?`, '提示').then(() => {
+        this.$http({
+          method: 'put',
+          url: '/comments/status',
+          params: { article_id: row.id },
+          data: { allow_comment: !row.comment_status }
+        }).then(() => {
+          this.getComments()
+        })
+      })
+    },
     getComments () {
       this.$http({
         url: '/articles',
@@ -50,5 +63,4 @@ export default {
 </script>
 
 <style>
-
 </style>
