68fbdd0a2ebf16e98cc68190d9d8e7a7cdfd677e
wb135 <wb135@yingu.com>
2019-08-24 10:37:53 +0800

    22.内容列表 列表数据展示

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -27,7 +27,6 @@
 
       <el-form-item label="时间选择">
         <el-date-picker
-          v-model="value1"
           style="width:450px;background-color:skyblue"
           type="daterange"
           range-separator="至"
@@ -35,24 +34,32 @@
           end-placeholder="结束日期"
         ></el-date-picker>
       </el-form-item>
-      <div class="total_title">
-        共找到56781条数据
-      </div>
+      <div class="total_title">共找到{{page.total}}条数据</div>
       <!-- 创建div列表 -->
       <div class="content-list" v-for="(item,index) in list" :key="index">
         <div class="content-item">
           <div class="left">
-            <img src="../../assets/img/404.png" alt="">
+            <img :src="item.cover.images[0]" alt />
             <div class="info">
-              <span>标题</span>
-              <el-tag style="width:60px">标签二</el-tag>
-              <span class="date">2019-08-22 17:21:31</span>
+              <span style="font-size:14px">{{item.title}}</span>
+              <el-tag
+                style="width:60px"
+                :type="item.status | statusType"
+              >{{item.status | statusText}}</el-tag>
+              <span class="date">{{item.pubdate}}</span>
             </div>
+          </div>
+          <div class="right">
+            <span>
+              <i class="el-icon-edit"></i>修改
+            </span>
+            <span>
+              <i class="el-icon-milk-tea"></i>删除
+            </span>
           </div>
         </div>
       </div>
     </el-form>
-
   </el-card>
 </template>
 
@@ -60,35 +67,83 @@
 export default {
   data () {
     return {
-      list: [1, 2, 3, 4, 5, 6],
+      list: [],
+      // 文章总数 默认为0
+      page: {
+        total: 0
+      },
       radio: 1,
       value: '',
       item: '',
-      options: [{
-        lable: '蛋糕🍰',
-        value: '选项1'
-      }, {
-        lable: '蛋挞🥚',
-        value: '选项2'
-      }, {
-        lable: '香蕉🍌',
-        value: '选项3'
-      }, {
-        lable: '苹果🍎',
-        value: '选项4'
-      }, {
-        lable: '榴莲🍊',
-        value: '选项5'
-      }]
-
+      options: [
+        {
+          lable: '蛋糕🍰',
+          value: '选项1'
+        },
+        {
+          lable: '蛋挞🥚',
+          value: '选项2'
+        },
+        {
+          lable: '香蕉🍌',
+          value: '选项3'
+        },
+        {
+          lable: '苹果🍎',
+          value: '选项4'
+        },
+        {
+          lable: '榴莲🍊',
+          value: '选项5'
+        }
+      ]
     }
+  },
+  methods: {
+    getArticles () {
+      this.$http({
+        url: '/articles'
+      }).then(res => {
+        this.list = res.data.results
+        this.page.total = res.data.total_count
+      })
+    }
+  },
+  filters: {
+    statusText (value) {
+      switch (value) {
+        case 0:
+          return '草稿'
+        case 2:
+          return '已发表'
+        case 3:
+          return '审核失败'
+        case 4:
+          return '已删除'
+      }
+    },
+    statusType (value) {
+      switch (value) {
+        case 0:
+          return 'warning'
+        case 2:
+          return 'success'
+        case 3:
+          return 'danger'
+        case 4:
+          return 'info'
+      }
+    }
+  },
+  created () {
+    this.getArticles()
   }
 }
 </script>
 
 <style lang='less' scoped>
 .total_title {
-  height:60px;
+  height: 60px;
   line-height: 60px;
   border-bottom: 1px dashed #ccc;
 }
@@ -102,30 +157,44 @@ export default {
       display: flex;
       align-items: center;
       img {
-        width:150px;
+        width: 150px;
         height: 100px;
         border-radius: 4px;
       }
       .info {
-        margin-left:10px;
+        margin-left: 10px;
         display: flex;
         height: 100px;
         padding: 5px 0;
         flex-direction: column;
         justify-content: space-between;
         .date {
-          color:#999;
-          font-size:12px;
+          color: #999;
+          font-size: 12px;
         }
       }
     }
     .right {
-      span,span i {
-        font-size:12px;
-        color:#333
+      display: flex;
+      align-items: center;
+      span,
+      span i {
+        font-size: 14px;
+        color: #333;
       }
       span {
-        margin-right: 5px;
+        border: 1px solid #ccc;
+
+        padding: 2px;
+        margin-right: 10px;
+        .el-icon-edit {
+          color: purple;
+          margin-right: 5px;
+        }
+        .el-icon-milk-tea {
+          color: red;
+          margin-right: 5px;
+        }
       }
     }
   }
